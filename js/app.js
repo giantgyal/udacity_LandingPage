c531acd6054a4042097e54104cc7465b55ecc9bb
TEJIRI <teejaycharles64@gmail.com>
2022-07-15 15:39:14 +0100

    second commit

--- js/app.js
+++ js/app.js
@@ -8,12 +8,9 @@ function generateNav(id, name){
     const nav = `<a class ="menu__link" data-id="${id}">${name}</a>`;
     return nav;
 };
-//Calculates size of sections
-const size = function (section){
-    return Math.floor(section.getBoundingClientRect().top);};
 
 // Build the menu 
-navigationBuild();
+navigationBuild()
 
 // build the nav
 function navigationBuild(){
@@ -30,38 +27,40 @@ function navigationBuild(){
     const navBarLi = document.getElementById('navbar__list')
     navBarLi.appendChild(fragment);
 };
+ //viewport active class(add & rem0ve function)
 
-// viewport active class(add & rem0ve function)
-const addActive = function(conditional, section){
-    if(conditional){
-        section.classList.add('your-active-class');
+ // activates section
+window.addEventListener('scroll', (event) => {
+
+    // create new variable for section again 
+    const sectionsLi = document.querySelectorAll('section');
+  
+    // looping through sections
+    sectionsLi.forEach( section => {
+  
+      // calculates size 
+      const size = section.getBoundingClientRect().top;
+
+      if (size > 300 && size < 350) {
+        const active = section.classList.add('activate');//add class active 
         //box shadow and border is created
         section.style.cssText = "box-shadow:30px 30px 50px 10px #ffb6c1;border-radius:2px;border:2px solid black;";
         //slice section ID 
         const sectionId= section.id.slice(7,8) -1;
         //adds color to background 
         navigation.childNodes[sectionId].style.cssText="background-color:#800080;";
-    };
-};
-
- const removeActive = function (section){
-    section.classList.remove('your-active-class');
-    const sectionId= section.id.slice(7,8) -1;
-    //removes background color when navigation is not active 
+        active(size(),section);
+        inActive(section);
+      } else {
+       const inActive = section.classList.remove('activate');//remove class active
+        const sectionId= section.id.slice(7,8) -1;
+        //removes background color when navigation is not active 
         navigation.childNodes[sectionId].style.cssText="background-color:#ffffff;";
          //box shadow is removed when section is inactive
     section.style.cssText = "box-shadow:none;border-radius:none";"border:1px solid grey";
-};
-const activateSection = function(){
-    for(section of sections) {
-        const elementSize = size(section);
-
-        inviewport = () => elementSize < 300 && elementSize >= -350;
-
-        removeActive(section);
-        addActive(inviewport(),section);
-    };
-};
+      }
+    });
+  });
 // adds Smooth scroll function
 function scrollToElement(event){
     if(event.target.nodeName === 'A'){
@@ -74,5 +73,3 @@ function scrollToElement(event){
 navigation.addEventListener('click', function(event){
     scrollToElement(event)
 })
-// activates sections
-window.addEventListener('scroll' ,activateSection);
